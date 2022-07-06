0f93bd42e88c6adf18d05e72df2dcfa73fba2361
JJ Brindamour <jjbrindamour@gmail.com>
2022-07-05 21:27:13 -0400

    Update Configurations

--- .config/bspwm/autostart.sh
+++ .config/bspwm/autostart.sh
@@ -16,16 +16,7 @@ function run {
 
 $HOME/.config/bspwm/polybar/launch.sh &
 
-#change your keyboard if you need it
-#setxkbmap -layout be
-
-keybLayout=$(setxkbmap -v | awk -F "+" '/symbols/ {print $2}')
-
-if [ $keybLayout = "be" ]; then
-  run sxhkd -c ~/.config/bspwm/sxhkd/sxhkdrc-azerty &
-else
-  run sxhkd -c ~/.config/bspwm/sxhkd/sxhkdrc &
-fi
+run sxhkd -c ~/.config/bspwm/sxhkd/sxhkdrc &
 
 #Some ways to set your wallpaper besides variety or nitrogen
 #feh --bg-scale ~/.config/bspwm/wall.png &
@@ -33,20 +24,11 @@ fi
 #feh --randomize --bg-fill ~/Képek/*
 #feh --randomize --bg-fill ~/Dropbox/Apps/Desktoppr/*
 
-dex $HOME/.config/autostart/arcolinux-welcome-app.desktop
 xsetroot -cursor_name left_ptr &
 
-#conky -c $HOME/.config/bspwm/system-overview &
-run variety &
-run nm-applet &
-run pamac-tray &
 run xfce4-power-manager &
-numlockx on &
-blueberry-tray &
 picom --config $HOME/.config/bspwm/picom.conf &
-/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
-/usr/lib/xfce4/notifyd/xfce4-notifyd &
-run volumeicon &
+xrandr -s 1440x900 & 
 nitrogen --restore &
 #run caffeine &
 #run vivaldi-stable &
