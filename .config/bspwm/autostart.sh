b2f806fa90799eb31a60f81a95c25ffbf30d98a1
JJ Brindamour <jjbrindamour@gmail.com>
2023-02-24 09:10:02 -0500

    update

--- .config/bspwm/autostart.sh
+++ .config/bspwm/autostart.sh
@@ -7,8 +7,6 @@ function run {
   fi
 }
 
-xrandr --output LVDS-1 --primary --mode 1366x768 --pos 117x900 --rotate normal --output VGA-1 --off --output HDMI-1 --mode 1600x900 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-2 --off --output HDMI-3 --off --output DP-2 --off --output DP-3 --off
-
 $HOME/.config/polybar/launch.sh &
 
 sxhkd -c ~/.config/bspwm/sxhkd/sxhkdrc &
